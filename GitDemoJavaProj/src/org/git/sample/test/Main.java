3e7b5ffb4f9e318f6ea524a3ec7be28e2292f1fa
Afroz <md.afroz.hcl@gmail.com>
2015-11-08 13:46:21 -0500

    initial commit

---
+++ GitDemoJavaProj/src/org/git/sample/test/Main.java
@@ -0,0 +1,12 @@
+package org.git.sample.test;
+
+public class Main {
+
+	public static void main(String[] argv){
+		Snacks snack = new Snacks();
+		System.out.println("Hello, this is a test.");
+		System.out.println("git test with eclipse plugin");
+		snack.funTime();
+	}
+	
+}
---
+++ GitDemoJavaProj/src/org/git/sample/test/Snacks.java
@@ -0,0 +1,8 @@
+package org.git.sample.test;
+
+public class Snacks {
+	public void funTime(){
+		System.out.println("Sample class");
+	}
+
+}
